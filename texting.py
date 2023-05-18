43d2bb735c21b45bbc9449ed67e8bf84b3b3ee47
sumanthmalipeddi <sumanth.9666@gmail.com>
2023-05-18 11:54:41 +0530

    trailing from vscode

--- texting.py
+++ texting.py
@@ -2,3 +2,4 @@
 #the file name
 print('new sheet has been created for the git hub purposes')
 print('now i am checking with CLI for learning in command prompts')
+print('I am editing in vscode and sending to git from the vscode')
