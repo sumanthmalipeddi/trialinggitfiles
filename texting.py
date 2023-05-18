c2ec0f4d28c5351a4e703896ba6860ef8f0e057a
sumanthmalipeddi <sumanth.9666@gmail.com>
2023-05-18 11:57:05 +0530

    trailing from cli interface

--- texting.py
+++ texting.py
@@ -3,3 +3,4 @@
 print('new sheet has been created for the git hub purposes')
 print('now i am checking with CLI for learning in command prompts')
 print('I am editing in vscode and sending to git from the vscode')
+print('I am editing in vscode and sending to git from cli interface')
